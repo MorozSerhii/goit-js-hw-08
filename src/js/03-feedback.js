62f123f139d19d09530df393510ff93d3c7d172b
Serhii <ecprap@gmail.com>
2023-01-29 17:26:09 +0200

    end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+initForm();
+let dataValues = {};
+const LOCALDATA_KEY = '"feedback-form-state"';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(dataValues);
+  dataValues = {};
+  localStorage.removeItem(LOCALDATA_KEY);
+  e.target.reset();
+});
+
+form.addEventListener('input', throttle(setObj, 500));
+
+function setObj(e) {
+  dataValues[e.target.name] = e.target.value;
+  const throttleSet = localStorage.setItem(
+    LOCALDATA_KEY,
+    JSON.stringify(dataValues)
+  );
+}
+
+function initForm() {}
+let dataUsers = localStorage.getItem(LOCALDATA_KEY);
+if (dataUsers) {
+  dataUsers = JSON.parse(dataUsers);
+  Object.entries(dataUsers).forEach(([name, value]) => {
+    dataUsers[name] = value;
+    form.elements[name].value = value;
+  });
+}
