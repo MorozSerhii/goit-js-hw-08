55845810bb1638c559ad85861d8861e88fbcd4c4
Serhii <ecprap@gmail.com>
2023-01-30 23:30:19 +0200

    end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('iframe');
 
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const wrightCurentTime = throttle(iframe => {
-  localStorage.setItem('videoplayer-current-time', `${iframe.seconds}`);
+const VIDEO_KEY = 'videoplayer-current-time';
+
+setVideoTime();
+
+const dataSetTime = throttle(function (iframe) {
+  localStorage.setItem(VIDEO_KEY, ` ${iframe.seconds}`);
 }, 1000);
 
-player.on('timeupdate', wrightCurentTime);
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', dataSetTime);
+function setVideoTime() {
+  const savedData = localStorage.getItem(VIDEO_KEY);
+  if (savedData) {
+    player.setCurrentTime(localStorage.getItem(VIDEO_KEY));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,35 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-initForm();
-let dataValues = {};
-const LOCALDATA_KEY = '"feedback-form-state"';
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  console.log(dataValues);
-  dataValues = {};
-  localStorage.removeItem(LOCALDATA_KEY);
-  e.target.reset();
-});
-
+form.addEventListener('submit', resetForm);
 form.addEventListener('input', throttle(setObj, 500));
+
+const LOCALDATA_KEY = 'feedback-form-state';
+
+let dataValues = {};
+initForm();
+
+function initForm() {
+  let dataUsers = localStorage.getItem(LOCALDATA_KEY);
+  if (dataUsers) {
+    dataUsers = JSON.parse(dataUsers);
+    Object.entries(dataUsers).forEach(([name, value]) => {
+      dataValues[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
 
 function setObj(e) {
   dataValues[e.target.name] = e.target.value;
-  const throttleSet = localStorage.setItem(
-    LOCALDATA_KEY,
-    JSON.stringify(dataValues)
-  );
+  localStorage.setItem(LOCALDATA_KEY, JSON.stringify(dataValues));
 }
 
-function initForm() {}
-let dataUsers = localStorage.getItem(LOCALDATA_KEY);
-if (dataUsers) {
-  dataUsers = JSON.parse(dataUsers);
-  Object.entries(dataUsers).forEach(([name, value]) => {
-    dataUsers[name] = value;
-    form.elements[name].value = value;
-  });
+function resetForm(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((value, name) => console.log(`${name}:`, value));
+  dataValues = {};
+  localStorage.removeItem(LOCALDATA_KEY);
+  e.target.reset();
 }
